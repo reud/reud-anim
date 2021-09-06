2537adc242b3b4f3ea806dce5c27b0f63ab65481
reud <mail@reud.net>
2021-09-06 23:28:43 +0900

    update summary

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
 	import Header from "./components/Header.svelte";
-
+	import StarRating from "./components/StarRating.svelte";
+	import { fade } from 'svelte/transition';
 	export let name: string;
 </script>
 
 <main>
 	<Header />
-	<div class="container-fluid" >
+	<div transition:fade="{{delay: 250}}" class="container-fluid" >
 		<h1 class="title">
 			reud the animation
 		</h1>
-		<span style="color: #768B98; font-size: 20px">シーズン1</span>
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+		<div class="d-flex">
+			<span  style="color: #768B98; font-size: 20px; margin-right: 20px">シーズン1</span>
+			<StarRating />
+			<span  style="color: #79b8f3; font-size: 20px; margin-left: 10px">(8,448)</span>
+			<span  style="color: #768B98; font-size: 14px; margin-left: 20px; margin-top: 5px;">2021</span>
+		</div>
+		<div class="summary-text">
+			どこにでも普通の青年が居た。様々な人に「reud」と呼ばれる少年はどこにでもある、平穏な日々を過ごしていた。
+			ある日青年に手紙と共に一枚のお面が届く。手紙を見るとそのお面を付けて仮面舞踏会に来ないかという誘いであった。
+			日常に嫌気が指していたreudは仮面舞踏会に出席することになる。その仮面舞踏会は奇妙であった。
+		</div>
+		<div>
+			<span  style="color: #768B98; font-size: 20px; margin-top: 120px; bottom: 20px;">出演 reud</span>
+			<br>
+			<span  style="color: #79b8f3; font-size: 50px; margin-right: 20px; bottom: 20px;">prime</span>
+			<span  style="color: white; font-weight: 700; font-size: 20px; margin-right: 20px; bottom: 20px;">シーズン1はプライム会員特典です</span>
+		</div>
 	</div>
 </main>
 
 <style>
+	.summary-text {
+		color: white;
+		font-size: 18px;
+		margin-top: 20px;
+		width: 800px;
+	}
 	body {
 	}
 	main {
@@ -26,7 +48,6 @@
 		width: 100vw;
 		height: 100vh;
 	}
-
 	.title {
 		margin-top: 60px;
 	}
---
+++ src/components/StarRating.svelte
@@ -0,0 +1,45 @@
+<div class="star-ratings-css">
+    <div class="star-ratings-css-top" style="width: 92%"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+    <div class="star-ratings-css-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+</div>
+<style>
+    @import url(http://fonts.googleapis.com/css?family=Open+Sans:400,600,700);
+    .star-ratings-css {
+        unicode-bidi: bidi-override;
+        color: #c5c5c5;
+        font-size: 25px;
+        position: relative;
+        text-shadow: 0px 1px 0 #a2a2a2;
+        bottom: 5px;
+
+    }
+    .star-ratings-css-top {
+        color: #e7711b;
+        padding: 0;
+        position: absolute;
+        z-index: 1;
+        display: block;
+        top: 0;
+        left: 0;
+        overflow: hidden;
+    }
+    .star-ratings-css-bottom {
+        padding: 0;
+        display: block;
+        z-index: 0;
+    }
+    body {
+        margin: 50px;
+        text-align: center;
+        font-family: 'Open Sans', sans-serif;
+        background: #f2fbff;
+    }
+    em {
+        font-style: italic;
+    }
+    span {
+        place-items: center;
+        margin-bottom: 10px;
+    }
+
+</style>
